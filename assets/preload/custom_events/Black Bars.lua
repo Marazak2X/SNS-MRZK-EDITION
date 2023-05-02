975cd6aa7a93f96417a2d5335c6eb299e27e1fcd
Marazak2X <123882434+Marazak2X@users.noreply.github.com>
2023-05-02 21:30:30 +0900

    up

--- assets/preload/custom_events/Black Bars.lua
+++ assets/preload/custom_events/Black Bars.lua
@@ -1,13 +1,11 @@
 
 function onCreatePost()
-	makeLuaSprite('UpperBar', 'empty', -110, -350)
-	makeGraphic('UpperBar', 1500, 350, '000000')
-	setObjectCamera('UpperBar', 'HUD')
+	makeLuaSprite('UpperBar', 'aBlackBar', -110, -350)
+	setObjectCamera('UpperBar', 'hudother')
 	addLuaSprite('UpperBar', false)
 
-	makeLuaSprite('LowerBar', 'empty', -110, 720)
-	makeGraphic('LowerBar', 1500, 350, '000000')
-	setObjectCamera('LowerBar', 'HUD')
+	makeLuaSprite('LowerBar', 'aBlackBar', -110, 720)
+	setObjectCamera('LowerBar', 'hudother')
 	addLuaSprite('LowerBar', false)
 
 	UpperBar = getProperty('UpperBar.y')
---
+++ assets/preload/stages/neo.lua
@@ -0,0 +1,5 @@
+function onCreate()
+	makeLuaSprite('bg','NeoCity',-1275,-275);
+	scaleObject('bg',1.25,1.25);
+	addLuaSprite('bg',false);
+end
